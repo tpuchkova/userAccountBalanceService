ae974d5a48f29a5c8dc367d065011a0e8fd039f9
Tatjana Putskova <tpuchkova@gmail.com>
2024-07-11 01:00:43 +0300

    refactor main

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 	"github.com/spf13/viper"
 	"log"
@@ -17,24 +18,21 @@ func main() {
 		log.Fatalf("error initializing config: %s", err.Error())
 	}
 
-	db, err := repository.NewPostgresDB(repository.Config{
-		Host:     os.Getenv("DATABASE_HOST"),
-		Port:     os.Getenv("DATABASE_PORT"),
-		Username: os.Getenv("DATABASE_USERNAME"),
-		DBName:   os.Getenv("DATABASE_NAME"),
-		SSLMode:  "disable",
-		Password: os.Getenv("DATABASE_PASSWORD"),
-	})
-	if err != nil {
-		log.Fatalf("failed to initialize db: %s", err.Error())
-	}
-
+	db := initializeDatabase()
 	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
-	srv := new(userAccountBalanceService.Server)
+	initializeTicker(services)
 
+	srv := new(userAccountBalanceService.Server)
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
+		log.Fatalf("error occured while running http server: %s", err.Error())
+	}
+
+}
+
+func initializeTicker(services *service.Service) {
 	ticker := time.NewTicker(time.Duration(viper.GetInt("cancelTransactionIntervalMinutes")) * time.Minute)
 
 	go func() {
@@ -48,11 +46,21 @@ func main() {
 			}
 		}
 	}()
+}
 
-	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
-		log.Fatalf("error occured while running http server: %s", err.Error())
+func initializeDatabase() *sqlx.DB {
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     os.Getenv("DATABASE_HOST"),
+		Port:     os.Getenv("DATABASE_PORT"),
+		Username: os.Getenv("DATABASE_USERNAME"),
+		DBName:   os.Getenv("DATABASE_NAME"),
+		SSLMode:  "disable",
+		Password: os.Getenv("DATABASE_PASSWORD"),
+	})
+	if err != nil {
+		log.Fatalf("failed to initialize db: %s", err.Error())
 	}
-
+	return db
 }
 
 func initConfig() error {
